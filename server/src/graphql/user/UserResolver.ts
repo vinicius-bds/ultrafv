54b200a4232f024cb73f2dac1acaaa9fa10384b4
Vinícius Barbosa dos Santos <64980353+vinicius-bds@users.noreply.github.com>
2021-03-03 09:00:55 -0300

    Recomeço

--- server/src/graphql/user/UserResolver.ts
+++
@@ -1,8 +0,0 @@
-import { Resolver, Query } from 'type-graphql'
-import { User } from './User'
-
-@Resolver(User)
-export class UserResolver {
-    @Query(() => [User])
-    async users() {}
-}
--- server/src/utils/connection.ts
+++
@@ -1,3 +0,0 @@
-import { createConnection } from 'typeorm'
-
-createConnection().then(() => console.log('Conectado com sucesso ao banco de dados')).catch(error => console.log(error))
